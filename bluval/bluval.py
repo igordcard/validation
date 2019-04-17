0469b07ecc803e7a0ba91474d916fa3cf5aacd4c
Naga Sugguna <ns156u@att.com>
2019-04-17 07:56:05 -0500

    bluval engine refactoring
    actual .robot testcases used instead of helloworld
    bluval-dummy.yaml is added for dummy blue print to test easily
    All the rebot results redirected to one directory results
    click is used to take parameters from CLI
    optional --layer/l is taken as parameter
    Deriving yaml location from blue print name
    Sections named as layers and layer names expanded hw -> hardware
    .py code refactored, mode functions added.
    
    Change-Id: Id916a79301d32ed64afc398fb486ba71b81a1dea
    Signed-off-by: Naga Sugguna <ns156u@att.com>

--- bluval/bluval.py
+++ bluval/bluval.py
@@ -15,48 +15,70 @@
 # See the License for the specific language governing permissions and        #
 # limitations under the License.                                             #
 ##############################################################################
-"""This module parses yaml file, reads sections, testcases and executes each
+"""This module parses yaml file, reads layers, testcases and executes each
 testcase
 """
 
 import subprocess
-import sys
+import click
 import yaml
 
 def run_testcase(testcase):
     """Runs a single testcase
     """
     show_stopper = testcase.get('show_stopper', False)
+    what = testcase.get('what')
+    results = "results/"+what
+    command = '{} {} {} {}'.format("robot", "-d", results, what)
 
     print('Executing testcase {}'.format(testcase['name']))
     print('          show_stopper {}'.format(show_stopper))
-    cmd = 'robot {}'.format(testcase['what'])
-    print('Invoking {}'.format(cmd))
+    print('Invoking {}'.format(command))
     try:
-        status = subprocess.call(cmd, shell=True)
+        status = subprocess.call(command, shell=True)
         if status != 0 and show_stopper:
             print('Show stopper testcase failed')
             return status
     except OSError:
-        print('Error while executing {}'.format(cmd))
+        print('Error while executing {}'.format(command))
         return -1
     return status
 
 
-def parse_yaml(testcase_loc):
-    """Parse yaml file and do tasks on each testcase
+def validate_layer(blueprint, layer):
+    """validates a layer by validating all testcases under that layer
     """
-    with open(testcase_loc) as testcase_file:
-        testcases = yaml.safe_load(testcase_file)
-    blueprint = testcases['blueprint']
-    for section in blueprint['sections']:
-        print('## Section {}'.format(section))
-        for testcase in blueprint[section]:
-            run_testcase(testcase)
+    print('## Layer {}'.format(layer))
+    for testcase in blueprint[layer]:
+        run_testcase(testcase)
+
+
+def validate_blueprint(yaml_loc, layer):
+    """Parse yaml file and validates given layer. If no layer given all layers
+    validated
+    """
+    with open(yaml_loc) as yaml_file:
+        yamldoc = yaml.safe_load(yaml_file)
+    blueprint = yamldoc['blueprint']
+    if layer is None:
+        for each_layer in blueprint['layers']:
+            validate_layer(blueprint, each_layer)
+    else:
+        validate_layer(blueprint, layer)
+
+
+@click.command()
+@click.argument('blueprint')
+@click.option('--layer', '-l')
+def main(blueprint, layer):
+    """Takes blueprint name and optional layer. Validates inputs and derives
+    yaml location from blueprint name. Invokes validate on blue print.
+    """
+    yaml_loc = 'bluval/bluval-{}.yaml'.format(blueprint)
+    layer = layer.lower()
+    validate_blueprint(yaml_loc, layer)
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print('usage: bluval.py <testcase.yaml>')
-        sys.exit(1)
-    parse_yaml(sys.argv[1])
+    # pylint: disable=no-value-for-parameter
+    main()
