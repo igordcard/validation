68de08ebfc5a37940cc30c64eebe70b3d4c8e237
Alexandru Antone <Alexandru.Antone@enea.com>
2020-01-07 12:44:41 +0200

    Remove docker-binary mount. Use container binary
    The docker client binary mounted from the host is not compatible
    with Alpine Linux for certain host distros.
    
    Signed-off-by: Alexandru Antone <Alexandru.Antone@enea.com>
    Change-Id: Iec10dea423adc32f2b9f651043e30a2aa02c7d76

--- bluval/blucon.sh
+++ bluval/blucon.sh
@@ -52,7 +52,6 @@ set -x
 
 docker run --rm \
     -v /var/run/docker.sock:/var/run/docker.sock \
-    -v /usr/bin/docker:/usr/bin/docker \
     -v $AKRAINO_HOME/results:/opt/akraino/results \
     -v $AKRAINO_HOME/validation:/opt/akraino/validation \
     akraino/validation:blucon-$image_tag "$@"
